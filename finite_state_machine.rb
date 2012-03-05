0748a97395cf0ee9e4a93ca4952c96063a011747
Guillermo Sandoval <gsandoval@darchitect.org>
2012-03-05 08:15:28 -0600

    Adds support to process non deterministic machines

--- finite_state_machine.rb
+++ finite_state_machine.rb
@@ -1,5 +1,6 @@
 class FiniteStateMachine
 	attr_accessor :states, :alphabet, :transitions, :initial_state, :final_states
+	
 	def initialize(s, l, d, q0, f)
 		@states = s
 		@alphabet = l
@@ -7,11 +8,31 @@ class FiniteStateMachine
 		@initial_state = q0
 		@final_states = f
 	end
-	def process(s)
-		q = @initial_state
-		s.each_char {|c| q = @transitions[q][c]}
-		@final_states.include? q
+	
+	def deterministic?
+		all_transitions_defined = true
+		@transitions.each do |k,t| 
+			@alphabet.each do |c| 
+				all_transitions_defined &= t.key? c
+			end
+		end
+		!@alphabet.include?('e') && @transitions.size == @states.size && all_transitions_defined
 	end
+	
+	def process s
+		q = [@initial_state]
+		
+		s.each_char do |c|
+			state_queue = Array.new(q)
+			q.clear
+			state_queue = add_epsilon_states state_queue
+			state_queue.each {|sq| q.push @transitions[sq][c] if @transitions[sq].key? c}
+		end
+		
+		q = add_epsilon_states q
+		q.any? {|s| @final_states.include? s}
+	end
+	
 	def combine(fsm)
 		nstates = @states.product fsm.states
 		nalphabet = (fsm.alphabet + @alphabet).uniq # If they are different all hell breaks loose
@@ -22,6 +43,7 @@ class FiniteStateMachine
 		ninitial_state = [@initial_state, fsm.initial_state]
 		FiniteStateMachine.new nstates, nalphabet, ntransitions, ninitial_state, nil
 	end
+	
 	def union(fsm)
 		nfsm = combine(fsm)
 		nfsm.final_states = @final_states.product fsm.states
@@ -29,21 +51,27 @@ class FiniteStateMachine
 		nfsm.final_states.uniq!
 		nfsm
 	end
+	
 	def intersection(fsm)
 		nfsm = combine(fsm)
 		nfsm.final_states = @final_states.product fsm.final_states
 		nfsm
 	end
+	
 	def to_s
 		lines = Array.new
 		lines.push "States:"
 		lines.push @states.map {|s| "\tq%s" % s.to_s}
+		calphabet = @alphabet
+		unless deterministic?
+			calphabet.push 'e'
+		end
 		lines.push "Alphabet:"
-		lines.push "\t%s" % @alphabet.zip(Array.new(@alphabet.length - 1, ",")).to_s
+		lines.push "\t%s" % calphabet.zip([","] * (calphabet.size-1)).to_s
 		lines.push "Transitions:"
 		@transitions.keys.sort.each do |k|
-			@alphabet.each do |l|
-				lines.push "\tq%s -%s-> q%s" % [k, l, @transitions[k][l]] 
+			calphabet.each do |l|
+				lines.push "\tq%s -%s-> q%s" % [k, l, @transitions[k][l]] if @transitions[k].key? l
 			end
 		end
 		lines.push "initial_state state:"
@@ -51,5 +79,23 @@ class FiniteStateMachine
 		lines.push "Final states:"
 		lines.push @final_states.map {|s| "\tq%s" % s.to_s}
 		lines.join "\n"
+	end
+	
+	private
+	
+	def add_epsilon_states(state_queue)
+		unless deterministic?
+			begin
+				added_states = []
+				state_queue.each do |a|
+					added_states.push(@transitions[a]['e']) if @transitions[a].key? 'e'
+				end
+				size_before = state_queue.size
+				state_queue += added_states
+				state_queue.uniq!
+				size_after = (state_queue.size)
+			end while size_before < size_after
+		end
+		state_queue
 	end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'finite_state_machine'
 
-s1 = Array.new(4) {|i| i}
+s1 = [0, 1, 2, 3]
 l1 = %w[a b]
 q01 = 0
 f1 = [1, 2]
@@ -10,8 +10,9 @@ d1 = {
 		2 => {'a' => 2, 'b' => 3},
 		3 => {'a' => 3, 'b' => 3},
 	}
+m1 = FiniteStateMachine.new s1, l1, d1, q01, f1
 	
-s2 = Array.new(2) { |i| i }
+s2 = [0, 1]
 l2 = %w[a b]
 q02 = 0
 f2 = [0]
@@ -19,17 +20,48 @@ d2 = {
 		0 => {'a' => 1, 'b' => 0},
 		1 => {'a' => 0, 'b' => 1},
 	}
-
-m1 = FiniteStateMachine.new s1, l1, d1, q01, f1
-puts "M1"
-puts m1
 m2 = FiniteStateMachine.new s2, l2, d2, q02, f2
-puts "M2"
-puts m2
+
+s3 = [0, 1, 2, 3, 4]
+l3 = %w[a b]
+q03 = 0
+f3 = [0, 4]
+d3 = {
+		0 => {'e' => 1},
+		1 => {'a' => 2},
+		2 => {'e' => 3},
+		3 => {'b' => 4},
+		4 => {'e' => 1},
+	}
+m3 = FiniteStateMachine.new s3, l3, d3, q03, f3
+
 str = "ababaa"
-outcome = m1.process str
+
+puts "#{"*"*5} M1 #{"*"*5}"
+puts m1
+puts
+
+puts "#{"*"*5} M2 #{"*"*5}"
+puts m2
+outcome = m2.process str
 puts "string #{str} accepted: #{outcome}"
-puts "M1 union M2:"
+puts
+
+puts "#{"*"*5} M1 union M2 #{"*"*5}"
 puts m1.union m2
-puts "M1 intersect M2:"
+puts
+
+puts "#{"*"*5} M1 intersect M2 #{"*"*5}"
 puts m1.intersection m2
+puts
+
+puts "#{"*"*5} M3 #{"*"*5}"
+puts m3
+puts "Is deterministic: #{m3.deterministic?}"
+outcome = m3.process ""
+puts "empty string accepted: #{outcome}"
+outcome = m3.process str
+puts "string #{str} accepted: #{outcome}"
+str = "abab"
+outcome = m3.process str
+puts "string #{str} accepted: #{outcome}"
